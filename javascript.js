0c6977d21b2027c32fcceec95e7caf7a8bb4a433
Christina Thai <christina.thai128@gmail.com>
2022-05-08 17:44:56 -0400

    Add random computer play

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// computerPlay: computer randomly selects rock, paper, or scissor
+
+// window asking "rock paper scissors?"
+// playerSelection: player select rock, paper, or scissor
+
+// outcome1: rock beats scissor
+// outcome2: paper beats rocks
+// outcome3: scissor beats paper
+// display "you win!" + outcome (above) if player wins
+// display "you lose!" if player loses
+// input is case-insensitive
+
+let values = ["rock", "paper", "scissors"];
+let computerPlay = values[Math.floor(Math.random() * 3)];
+
+console.log(computerPlay)
+
+
+
