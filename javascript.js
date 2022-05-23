6eb23c7bb81f690cddc8ce564d2fe6e0e27aa56c
Christina Thai <christinathai7@gmail.com>
2022-05-23 12:02:48 -0400

    add rock paper scissor

--- javascript.js
+++ javascript.js
@@ -1,40 +1,53 @@
-// computer randomly selects rock, paper, or scissor
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
 
-// window asking "rock paper scissors?"
-// player select rock, paper, or scissor
-// input is case-insensitive
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
 
-// rock vs rock - tie
-// rock vs paper - you lose
-// rock vs scissors - you win
-// paper vs rock - you win
-// paper vs paper - tie
-// paper vs scissors - you lose
-// scissors vs rock - you lose
-// scissors vs paper - you win
-// scissors vs scissors - tie
-
-function computerPlay() {
-    let values = ["rock", "paper", "scissors"];
-    return values[Math.floor(Math.random() * 3)];
-}
-console.log(computerPlay())
-let computerSelection = computerPlay();
-
-let playerChoice = prompt("Rock, paper, or scissors?");
-let playerSelection = playerChoice.toLowerCase()
-console.log(playerSelection)
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return alert("Tie!");
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
     }
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return alert("You Lose! Paper beats Rock");
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
     }
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        return alert("You win!");
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
     }
+    computerChoiceDisplay.innerHTML = computerChoice
 }
 
-
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw!"
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = "You win!"
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = "You lose!"
+    }
+    if (computerChoice ===  'paper' && userChoice === 'scissors') {
+        result = "You win!"
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = "You lose!"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = "You win!"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = "You lose!"
+    }
+    resultDisplay.innerHTML = result
+}
